f676abd146b2ddc5be7c5d11386d5164be906d1e
ScarWu <xneriscool@gmail.com>
2014-03-20 20:15:58 +0800

    update html tester

--- test/HTMLPackTest.php
+++ test/HTMLPackTest.php
@@ -41,6 +41,9 @@ class HTMLPackTest extends PHPUnit_Framework_TestCase
             ], [
                 '< img src = "001.jpg" / >',
                 '<img src="001.jpg"/>'
+            ], [
+                ' < a href = "scar.simcz.tw" target = "_blank" style = " display : block ; font-size : 1.25em ; " > < /a >',
+                '<a href="scar.simcz.tw" target="_blank" style="display:block;font-size:1.25em;"></a>'
             ]
         ];
     }
@@ -49,11 +52,17 @@ class HTMLPackTest extends PHPUnit_Framework_TestCase
     {
         return [
             [
-                "",
-                ''
+                ' < div > hello < /div > ',
+                '<div>hello</div>'
             ], [
-                "",
-                ''
+                ' < div >  < div > hello < /div >  < /div > ',
+                '<div><div>hello</div></div>'
+            ], [
+                " < script type = \"text/javascript\" > ( function ( ) {  var po = document.createElement( 'script' ) ; po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName ( 'script' ) [ 0 ] ; s.parentNode.insertBefore ( po , s ) ; } ) ( ) ; < /script > ",
+                "<script type=\"text/javascript\">(function(){var po=document.createElement('script');po.type='text/javascript';po.async=true;po.src='https://apis.google.com/js/plusone.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(po,s);})();</script>"
+            ], [
+                " < style type = \"text/css\" > div { background-color : #FF0000 ; } < /style > ",
+                "<style type=\"text/css\">div{background-color:#FF0000;}</style>"
             ]
         ];
     }
@@ -62,11 +71,14 @@ class HTMLPackTest extends PHPUnit_Framework_TestCase
     {
         return [
             [
-                "",
-                ''
+                " \n< div >\n\t< img / >\n< /div >\n ",
+                '<div><img/></div>'
             ], [
-                "",
-                ''
+                " \r< div >\r\t< img / >\r< /div >\r ",
+                '<div><img/></div>'
+            ], [
+                " \r\n< div >\r\n\t< img / >\r\n< /div >\r\n ",
+                '<div><img/></div>'
             ]
         ];
     }
